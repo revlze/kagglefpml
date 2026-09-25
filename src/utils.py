a614ff1529a5f3363d3ab67565a05f4decc7a950
Platon U. <revlze@yahoo.com>
2026-09-25 18:21:18 +0300

    feat(utils.py): add set_seed func

---
+++ src/utils.py
@@ -0,0 +1,20 @@
+import os
+import random
+
+import torch
+import numpy as np
+
+
+def set_seed(seed=42):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    torch.use_deterministic_algorithms(True, warn_only=True)
+
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+
+    print(f'Setted seed to {seed}')
